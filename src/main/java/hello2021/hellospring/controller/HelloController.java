bdcac6657c3db604c87e795b64a5640782d19048
Gatsjy <gkswndks123@naver.com>
2021-01-02 21:54:31 +0900

    2021-01-02, 한주안, 인프런 스프링 입문 시작!

---
+++ src/main/java/hello2021/hellospring/controller/HelloController.java
@@ -0,0 +1,29 @@
+package hello2021.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Gatsjy
+ * @since 2021-01-02
+ * realize dreams myself
+ * Blog : https://blog.naver.com/gkswndks123
+ * Github : https://github.com/gatsjy
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+}
